669a8534c8f2c7494a5bdc710a6e8d4173f3de4a
KAI-MIN CHANG <k.m.chang@host-250.55-14-119.dynamic.totalbb.net.tw>
2016-12-17 11:34:01 +0800

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
+@user = User.create(email: "test@gmail.com", password: "asdfasdf",
+                    password_confirmation: "asdfasdf", first_name: "kevin", last_name: "Chang")
+
 100.times do |post|
-  Post.create(date: Date.today, rationale: "#{post} rationale content")
+  Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "100 Posts have been created"
