d645c4b33056c4c5cdc1e2ec6539664f2c9939d2
KAI-MIN CHANG <k.m.chang@host-250.55-14-119.dynamic.totalbb.net.tw>
2016-12-17 14:36:45 +0800

    Add new entry nav

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -27,6 +27,16 @@ describe "navigate" do
       expect(page).to have_content(/rationale|content/)
     end
   end
+
+  context "new" do
+    it 'has a link from the homepage' do
+      visit root_path
+
+      click_link("new_post_from_nav")
+      expect(page.status_code).to eq(200)
+    end
+  end
+
   context "creation" do
     before do
       visit new_post_path
